bd5a0f12f143c2e94b64cdc7598d401e65898207
makho-kukhianidze <94704279+makho-kukhianidze@users.noreply.github.com>
2021-12-21 18:40:00 +0400

    Add files via upload

---
+++ assets/js/scroll-top-button-and-menu.js
@@ -0,0 +1,35 @@
+// menu script - Start
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "12px";
+}
+// menu script - End
+
+// scroll top button - Start
+var mybutton = document.getElementById("Back-to-top-Button");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// scroll top button - End
+
+
+
+
+
+
